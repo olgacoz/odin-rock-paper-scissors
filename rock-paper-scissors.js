e35fa67c29b99418e00250bda8701dfdc0692100
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2025-12-02 14:41:20 +0200

    Add logic to play entire game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,10 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3); // Pick a random integer value between [0, 2]
@@ -26,26 +20,47 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        console.log('You win! Rock beats Scissors');
-        humanScore += 1;
-    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-        console.log('You lose! Paper beats Rock');
-        computerScore += 1;
-    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        console.log('You win! Paper beats Rock');
-        humanScore += 1;
-    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        console.log('You lose! Scissors beats Paper');
-        computerScore += 1;
-    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log('You win! Scissors beats Paper');
-        humanScore += 1;
-    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        console.log('You lose! Rock beats Scissors');
-        computerScore += 1;
-    } else { // they both pick the same action. No points for both
-        console.log(`Draw! You both choose ${humanChoice}`);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    console.log(`Human score: ${humanScore}   Computer score: ${computerScore}`);
+
+    if (humanScore > computerScore) {
+        console.log('Human wins!');
+    } else if (humanScore < computerScore) {
+        console.log('Computer wins!');
+    } else {
+        console.log('It\'s a draw!');
+    }
+    
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            console.log('You win! Rock beats Scissors');
+            humanScore += 1;
+        } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+            console.log('You lose! Paper beats Rock');
+            computerScore += 1;
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            console.log('You win! Paper beats Rock');
+            humanScore += 1;
+        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            console.log('You lose! Scissors beats Paper');
+            computerScore += 1;
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            console.log('You win! Scissors beats Paper');
+            humanScore += 1;
+        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            console.log('You lose! Rock beats Scissors');
+            computerScore += 1;
+        } else { // they both pick the same action. No points for both
+            console.log(`Draw! You both choose ${humanChoice}`);
+        }
     }
 }
